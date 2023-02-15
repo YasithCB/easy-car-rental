4f839f162665423e1a9696103de63830fa92bab8
YasithCB <yasith.cb2001@gmail.com>
2023-02-15 11:53:45 +0530

    WebAppInitializer.java extended with AbstractAnno..

--- back-end-spring/src/main/java/lk/easycarrent/spring/WebAppInitializer.java
+++ back-end-spring/src/main/java/lk/easycarrent/spring/WebAppInitializer.java
@@ -1,10 +1,26 @@
 package lk.easycarrent.spring;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * author  Yasith C Bandara
  * created 2/15/2023 - 11:33 AM
  * project back-end-spring
  */
 
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
