3ededa3ae126e5812d089ce053053bed5710f60a
YasithCB <yasith.cb2001@gmail.com>
2023-02-15 13:18:35 +0530

    driver entity added

---
+++ back-end-spring/src/main/java/lk/easycarrent/spring/entity/Driver.java
@@ -0,0 +1,29 @@
+package lk.easycarrent.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * author  Yasith C Bandara
+ * created 2/15/2023 - 1:08 PM
+ * project back-end-spring
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Driver {
+    @Id
+    private Long id;
+    private String name;
+    private String contact;
+    private String email;
+    private String address;
+}
