042ca92b0a18bcb955ab30f2888de95d39f526c5
YasithCB <yasith.cb2001@gmail.com>
2023-02-16 23:17:08 +0530

    DriverService and Impl classes created

---
+++ back-end-spring/src/main/java/lk/easycarrent/spring/service/DriverService.java
@@ -0,0 +1,13 @@
+package lk.easycarrent.spring.service;
+
+import lk.easycarrent.spring.dto.DriverDTO;
+
+/**
+ * author  Yasith C Bandara
+ * created 2/16/2023 - 11:14 PM
+ * project back-end-spring
+ */
+
+public interface DriverService {
+    void saveDriver(DriverDTO dto);
+}
---
+++ back-end-spring/src/main/java/lk/easycarrent/spring/service/impl/DriverServiceImpl.java
@@ -0,0 +1,33 @@
+package lk.easycarrent.spring.service.impl;
+
+import lk.easycarrent.spring.dto.DriverDTO;
+import lk.easycarrent.spring.entity.Driver;
+import lk.easycarrent.spring.repo.DriverRepo;
+import lk.easycarrent.spring.service.DriverService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+/**
+ * author  Yasith C Bandara
+ * created 2/16/2023 - 11:14 PM
+ * project back-end-spring
+ */
+
+@Service
+@Transactional
+public class DriverServiceImpl implements DriverService {
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private DriverRepo repo;
+
+    @Override
+    public void saveDriver(DriverDTO dto) {
+        repo.save(mapper.map(dto, Driver.class));
+    }
+}
