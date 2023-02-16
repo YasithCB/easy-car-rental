04eaa4ee70061ee30c636ad823fe2e0526bb12d2
YasithCB <yasith.cb2001@gmail.com>
2023-02-16 23:10:07 +0530

    DriverCOntroller created

---
+++ back-end-spring/src/main/java/lk/easycarrent/spring/controller/DriverController.java
@@ -0,0 +1,19 @@
+package lk.easycarrent.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author  Yasith C Bandara
+ * created 2/16/2023 - 11:08 PM
+ * project back-end-spring
+ */
+
+@RestController
+@CrossOrigin
+@RequestMapping("driver")
+public class DriverController {
+
+
+}
