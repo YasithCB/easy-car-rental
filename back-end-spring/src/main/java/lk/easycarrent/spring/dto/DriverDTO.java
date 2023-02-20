6cf9df3f2b2db707d9ae4ae13369ad7f2f4875dd
YasithCB <yasith.cb2001@gmail.com>
2023-02-20 13:27:55 +0530

    paymentDTO and service and repo classes initialized

--- back-end-spring/src/main/java/lk/easycarrent/spring/dto/DriverDTO.java
+++ back-end-spring/src/main/java/lk/easycarrent/spring/dto/DriverDTO.java
@@ -6,11 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
-import javax.persistence.CascadeType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import java.util.List;
 
 /**
