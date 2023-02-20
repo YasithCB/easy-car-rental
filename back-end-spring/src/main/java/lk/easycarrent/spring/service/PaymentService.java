2675ac295df4135162ee2fa8e03f0c892a172559
YasithCB <yasith.cb2001@gmail.com>
2023-02-20 13:33:10 +0530

    PaymentService class missed annotations added

--- back-end-spring/src/main/java/lk/easycarrent/spring/service/PaymentService.java
+++ back-end-spring/src/main/java/lk/easycarrent/spring/service/PaymentService.java
@@ -1,10 +1,15 @@
 package lk.easycarrent.spring.service;
 
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+
 /**
  * author  Yasith C Bandara
  * created 2/20/2023 - 1:25 PM
  * project back-end-spring
  */
 
+@Service
+@Transactional
 public interface PaymentService {
 }
