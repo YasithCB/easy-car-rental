08feda488618e06dbcec77d54f789256ba7972ca
YasithCB <yasith.cb2001@gmail.com>
2023-02-20 13:24:03 +0530

    paymentController class created

---
+++ back-end-spring/src/main/java/lk/easycarrent/spring/controller/PaymentController.java
@@ -0,0 +1,17 @@
+package lk.easycarrent.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author  Yasith C Bandara
+ * created 2/20/2023 - 1:22 PM
+ * project back-end-spring
+ */
+
+@RestController
+@CrossOrigin
+@RequestMapping("payment")
+public class PaymentController {
+}
