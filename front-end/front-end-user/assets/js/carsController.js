b567a80f7d0cfb202705fbea3e5c5bd4a9e407b0
YasithCB <yasith.cb2001@gmail.com>
2023-05-26 14:10:05 +0530

    images uploaded tp db and they are loading to home

--- front-end/front-end-user/assets/js/carsController.js
+++ front-end/front-end-user/assets/js/carsController.js
@@ -4,38 +4,42 @@ var selectedCar
 
 $("#reserveCarMain").css("display", "none")
 
-$.ajax({
-    url: baseURL + "car",
-    dataType: "json",
-    success: function (resp) {
-        let carId = 0;
+loadCars();
 
-        let generalCount = 0
-        let premiumCount = 0
-        let luxuryCount = 0
-        for (let car of resp.data) {
-            carId = car.id
+function loadCars() {
+    $.ajax({
+        url: baseURL + "car",
+        dataType: "json",
+        success: function (resp) {
+            let carId = 0;
 
-            if (car.isAvailable === true) {
-                let listItem = "<li><div class=\"featured-car-card\"><figure class=\"card-banner\"><img src=\"./assets/images/car-1.jpg\" alt=\"car-image\" loading=\"lazy\" width=\"440\" height=\"300\"class=\"w-100\"></figure><div class=\"card-content\"><div class=\"card-title-wrapper\"><h6 class=\"h6 card-title\"><a href=\"#\">" + car.brand + "</a></h6><data class=\"year\" value=\"2021\">" + car.yom + "</data></div><ul class=\"card-list\"><li class=\"card-list-item\"><ion-icon name=\"color-palette-outline\"></ion-icon><span class=\"card-item-text\">" + car.color + "</span></li><li class=\"card-list-item\"><ion-icon name=\"flash-outline\"></ion-icon><span class=\"card-item-text\">" + car.fuelType + "</span></li><li class=\"card-list-item\"><ion-icon name=\"speedometer-outline\"></ion-icon><span class=\"card-item-text\"> " + car.kmPerLitre + "km / 1-litre</span></li><li class=\"card-list-item\"><ion-icon name=\"hardware-chip-outline\"></ion-icon><span class=\"card-item-text\">" + car.transmissionType + "</span> </li> <li class=\"card-list-item\"><ion-icon name=\"cash-outline\"></ion-icon><span class=\"card-item-text\">" + car.monthlyRate + "/month</span> <li class=\"card-list-item\"><ion-icon name=\"leaf-outline\"></ion-icon><span class=\"card-item-text\">" + car.freeMileage + "km/free</span> <li class=\"card-list-item\"><ion-icon name=\"card-outline\"></ion-icon><span class=\"card-item-text\">" + car.chargeForExtraKm + "/extra Km</span> </li></ul><div class=\"card-price-wrapper\"><p class=\"card-price\"><strong>" + car.dailyRate + " LKR" + "</strong> / day</p><button class=\"btn fav-btn\" aria-label=\"Add to favourite list\"><ion-icon name=\"heart-outline\"></ion-icon></button><button id=\" " + carId + " \" class=\"btn btn-primary rentNowOnCard\" onClick=\"rentNowClick(this.id)\">Rent now</button></div></div></div></li>"
+            let generalCount = 0
+            let premiumCount = 0
+            let luxuryCount = 0
+            for (let car of resp.data) {
+                carId = car.id
 
-                if (car.carType.toLowerCase() === "general" && generalCount < 3) {
-                    generalCount++
-                    $("#generalCars").append(listItem);
-                } else if (car.carType.toLowerCase() === "premium" && premiumCount < 3) {
-                    premiumCount++
-                    $("#premiumCars").append(listItem);
-                } else if (car.carType.toLowerCase() === "luxury" && luxuryCount < 3) {
-                    luxuryCount++
-                    $("#luxuryCars").append(listItem);
+                if (car.isAvailable === true) {
+                    let listItem = `<li><div class=\"featured-car-card\"><figure class=\"card-banner\"><img src= \"${car.imageFront}\" alt=\"car-image\" loading=\"lazy\" width=\"440\" height=\"300\"class=\"w-100\"></figure><div class=\"card-content\"><div class=\"card-title-wrapper\"><h6 class=\"h6 card-title\"><a href=\"#\"> ${car.brand} </a></h6><data class=\"year\" value=\"2021\"> ${car.yom} </data></div><ul class=\"card-list\"><li class=\"card-list-item\"><ion-icon name=\"color-palette-outline\"></ion-icon><span class=\"card-item-text\"> ${car.color} </span></li><li class=\"card-list-item\"><ion-icon name=\"flash-outline\"></ion-icon><span class=\"card-item-text\"> ${car.fuelType} </span></li><li class=\"card-list-item\"><ion-icon name=\"speedometer-outline\"></ion-icon><span class=\"card-item-text\"> ${car.kmPerLitre} km / 1-litre</span></li><li class=\"card-list-item\"><ion-icon name=\"hardware-chip-outline\"></ion-icon><span class=\"card-item-text\"> ${car.transmissionType} </span> </li> <li class=\"card-list-item\"><ion-icon name=\"cash-outline\"></ion-icon><span class=\"card-item-text\"> ${car.monthlyRate}/month</span> <li class=\"card-list-item\"><ion-icon name=\"leaf-outline\"></ion-icon><span class=\"card-item-text\"> ${car.freeMileage}km/free </span> <li class=\"card-list-item\"><ion-icon name=\"card-outline\"></ion-icon><span class=\"card-item-text\"> ${car.chargeForExtraKm}/extra Km</span> </li></ul><div class=\"card-price-wrapper\"><p class=\"card-price\"><strong> ${car.dailyRate} LKR</strong> / Day</p><button class=\"btn fav-btn\" aria-label=\"Add to favourite list\"><ion-icon name=\"heart-outline\"></ion-icon></button><button id= \"${carId}\" class=\"btn btn-primary rentNowOnCard\" onClick=\"rentNowClick(this.id)\">Rent now</button></div></div></div></li>`
+
+                    if (car.carType.toLowerCase() === "general" && generalCount < 3) {
+                        generalCount++
+                        $("#generalCars").append(listItem);
+                    } else if (car.carType.toLowerCase() === "premium" && premiumCount < 3) {
+                        premiumCount++
+                        $("#premiumCars").append(listItem);
+                    } else if (car.carType.toLowerCase() === "luxury" && luxuryCount < 3) {
+                        luxuryCount++
+                        $("#luxuryCars").append(listItem);
+                    }
                 }
             }
+        },
+        error: function (error) {
+            alert(JSON.parse(error.responseText).message);
         }
-    },
-    error: function (error) {
-        alert(JSON.parse(error.responseText).message);
-    }
-});
+    });
+}
 
 function rentNowClick(carId) {
     window.scrollTo(0, 0)
